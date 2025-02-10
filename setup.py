7dc7d871d54614b454ef620c44123d0692befc8a
Elham Mahmoudi <111046813+elimah91@users.noreply.github.com>
2025-02-10 16:19:57 -0600

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,23 +1,23 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='roi2bb', 
-    version='0.1.0', 
-    description='A tool to convert coordinates of 3D Slicer JSON annotations to YOLO format.', 
-    url='https://github.com/elimah91/ROI2BB', 
-    author='Elham Mahmoudi',  
-    author_email='mahmoudi.elham91@gmail.com', 
-    license='MIT',  
-    packages=find_packages(), 
-    python_requires=">=3.7"
-    classifiers=[
-        "Programming Language :: Python :: 3.x",
-        "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent",
+    name="roi2bb",
+    version="0.1.0",
+    packages=find_packages(),
+    install_requires=[
+        "numpy",
+        "pandas",
+        "opencv-python",
+        "pydicom",
+        "nibabel",
+        "Pillow",
+        "SimpleITK"
     ],
+    python_requires=">=3.7",
     entry_points={
         "console_scripts": [
             "roi2bb=roi2bb.converter:main"
         ],
     },
 )
+
