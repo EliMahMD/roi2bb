0ab1225869447adbf1d14d57cdf7bace41110429
Elham Mahmoudi <111046813+elimah91@users.noreply.github.com>
2025-02-10 13:03:14 -0600

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,17 +1,20 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='roi2bb',  # Your package name
-    version='0.1.0',  # The initial release version
-    description='Convert ROIs from 3D Slicer JSON format to YOLO bounding boxes',  # Short description of your package
-    long_description=open('README.md').read(),  # You can use your README as a long description
-    long_description_content_type='text/markdown',  # This is important if you're using Markdown for the long description
-    url='https://github.com/elimah91/ROI2BB',  # The URL of your project's homepage or repository
-    author='Your Name',  # Your name
-    author_email='your.email@example.com',  # Your email address
-    license='MIT',  # License under which your code is distributed
-    packages=find_packages(),  # Automatically find packages in your project
-    install_requires=[  # External packages that your project depends on
+    name='roi2bb', 
+    version='0.1.0', 
+    description='A tool to convert coordinates of 3D Slicer JSON annotations to YOLO format.', 
+    url='https://github.com/elimah91/ROI2BB', 
+    author='Elham Mahmoudi',  
+    author_email='mahmoudi.elham91@gmail.com', 
+    license='MIT',  
+    packages=find_packages(), 
+    classifiers=[
+    "Programming Language :: Python :: 3",
+    "License :: OSI Approved :: MIT License",
+    "Operating System :: OS Independent",
+    ],
+    install_requires=[  
         'nibabel',
         'json',
         'numpy',
